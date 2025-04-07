ce21078eeb6e314332a49528f1b1c1c53bd150fe
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2025-04-08 02:01:28 +0530

    chore(theme): re-add shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
-    <main>
-      <Button>Click ShadCn</Button>
+    <main className="h-screen flex justify-center items-center">
+      <Button>Startalyse</Button>
     </main>
   );
 }
