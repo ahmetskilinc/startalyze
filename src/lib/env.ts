1b6dd01a0413f4ca935a30c37439ab4bf2a827fc
Ahmet Kilinc <akx9@icloud.com>
2025-04-07 21:51:54 +0100

    feat: added better-auth

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,8 +4,14 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+    BETTER_AUTH_URL: z.string().url(),
+  },
+  client: {
+    NEXT_PUBLIC_BETTER_AUTH_URL: z.string().url(),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   },
 });
---
+++ src/server/auth/index.ts
@@ -0,0 +1,44 @@
+import { cache } from "react";
+import { env } from "@/lib/env";
+import { db } from "@/server/db";
+import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+import { openAPI } from "better-auth/plugins";
+import { headers } from "next/headers";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  trustedOrigins: [env.BETTER_AUTH_URL],
+  plugins: [openAPI(), nextCookies()],
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    updateAge: 60 * 60 * 24,
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60,
+    },
+  },
+  rateLimit: {
+    window: 60,
+    max: 5,
+  },
+  account: {
+    accountLinking: {
+      enabled: true,
+      trustedProviders: ["github"],
+    },
+  },
+} satisfies BetterAuthOptions);
+
+export const getServerSession = cache(
+  async () =>
+    await auth.api.getSession({
+      headers: await headers(),
+    }),
+);
+
+export type Session = typeof auth.$Infer.Session;
+export type AuthUserType = Session["user"];
