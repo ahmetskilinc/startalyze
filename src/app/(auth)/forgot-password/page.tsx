8bbe2ba9d07e7fb971bd0d71af90bef36eb06a7c
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2025-04-08 18:04:54 +0530

    chore: apply formatting fixes to exports

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,8 +1,6 @@
 import { ForgotPasswordForm } from "@/app/components/auth/forgotPass-form";
 import React from "react";
 
-const ForgotPassword = () => {
+export default function ForgotPassword() {
   return <ForgotPasswordForm />;
-};
-
-export default ForgotPassword;
+}
