f58f082ade85c225d35183acec9ca7601204036f
Ahmet Kilinc <akx9@icloud.com>
2025-04-11 13:48:51 +0100

    fix: fix domain

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,12 @@
+import { env } from "./env";
+
 export const domain =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
     : process.env.VERCEL_BRANCH_URL
-      ? `https://${process.env.VERCEL_BRANCH_URL}`
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
       : process.env.NEXT_PUBLIC_VERCEL_URL
         ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
         : process.env.NEXT_PUBLIC_DEVELOPMENT_URL
           ? process.env.NEXT_PUBLIC_DEVELOPMENT_URL
-          : "http://localhost:3000";
+          : env.NEXT_PUBLIC_DEVELOPMENT_URL;
