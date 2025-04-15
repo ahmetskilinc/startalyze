73cfccfb567288ff3ed530b6de2dd13f0cad4893
Ahmet Kilinc <akx9@icloud.com>
2025-04-15 17:54:53 +0100

    feat: refactoring signin signup

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -4,6 +4,7 @@ export const signUpSchema = z.object({
   name: z.string().min(3).max(50),
   email: z.string().email(),
   password: z.string().min(8),
+  confirmPassword: z.string().min(8),
 });
 
 export const signInSchema = z.object({
